2537e087dfa56e26590c53a9478dfa6544cab0b3
rexlx <rxlx@nullferatu.com>
2024-03-10 11:02:37 -0500

    better in dark

--- htmx.go
+++ htmx.go
@@ -122,7 +122,7 @@ var homePage = `
 	<script src="https://unpkg.com/htmx.org@1.9.6" integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni" crossorigin="anonymous"></script>
 </head>
 <body>
-<section class="section">
+<section class="section has-background-dark">
 	<div class="container">
 	<form hx-post="/poll">
 		%s
